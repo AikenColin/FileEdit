51eae29a7f287069e4498517c81657930bcb753e
AikenColin <42322337+AikenColin@users.noreply.github.com>
2018-08-12 20:52:23 -0500

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -30,8 +30,6 @@ namespace WindowsFormsApplication1
             aTimer.Elapsed += new System.Timers.ElapsedEventHandler(OnTimedEvent);
             aTimer.Interval = 5000;
             aTimer.Enabled = true;
-
-            CreateMyStatusBar();
         }
 
         private void OnTimedEvent(object source, System.Timers.ElapsedEventArgs e)
@@ -116,7 +114,6 @@ namespace WindowsFormsApplication1
             {
                 using (StreamReader sr = new StreamReader(@"C:\Users\colin\Desktop\File.txt"))
                 {
-
                     {
                         string line;
 
@@ -442,54 +439,5 @@ namespace WindowsFormsApplication1
                 MessageBox.Show(ex.ToString());
             }
         }
-
-        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void CreateMyStatusBar()
-        {
-            // Create a StatusBar control.
-            StatusBar statusBar1 = new StatusBar();
-            // Create two StatusBarPanel objects to display in the StatusBar.
-            StatusBarPanel panel1 = new StatusBarPanel();
-            StatusBarPanel panel2 = new StatusBarPanel();
-
-            // Display the first panel with a sunken border style.
-            panel1.BorderStyle = StatusBarPanelBorderStyle.Sunken;
-            // Initialize the text of the panel.
-            panel1.Text = "Checking for Emails...";
-            // Set the AutoSize property to use all remaining space on the StatusBar.
-            panel1.AutoSize = StatusBarPanelAutoSize.Spring;
-
-            // Display the second panel with a raised border style.
-            panel2.BorderStyle = StatusBarPanelBorderStyle.Raised;
-
-            // Create ToolTip text that displays time the application was 
-            //started.
-            panel2.ToolTipText = "Started: " + System.DateTime.Now.ToShortTimeString();
-            // Set the text of the panel to the current date.
-            panel2.Text = System.DateTime.Today.ToLongDateString();
-            // Set the AutoSize property to size the panel to the size of the contents.
-            panel2.AutoSize = StatusBarPanelAutoSize.Contents;
-
-            // Display panels in the StatusBar control.
-            statusBar1.ShowPanels = true;
-
-            // Add both panels to the StatusBarPanelCollection of the StatusBar.			
-            statusBar1.Panels.Add(panel1);
-            statusBar1.Panels.Add(panel2);
-
-            // Add the StatusBar to the form.
-            this.Controls.Add(statusBar1);
-        }
-
-        private void listBox1_SelectedIndexChanged_1(object sender, EventArgs e)
-        {
-
-        }
-
-
     }
 }
